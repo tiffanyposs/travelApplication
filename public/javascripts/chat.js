c1a19aca23cccf8d21686af6f019a4b436977ae3
tiffanyposs <tiffanyposs@gmail.com>
2015-04-19 19:39:20 -0400

    websocket host

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,7 +3,9 @@ $('#group_chat').click(function(){
 console.log(current_trip)
 
 
-var client = new WebSocket("ws://localhost:2000/" + current_trip);
+// var client = new WebSocket("ws://localhost:2000/" + current_trip);
+var client = new WebSocket("ws://tripppper.com:2000/" + current_trip);
+
 console.log(client)
 //use this code when you're ready to host online
 //var client = new WebSocket("ws://[your-url]:3000");
